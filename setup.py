3944e62b48dfbf01397e3f1a51d779c6401a043e
Roxey <32243415+roxey804@users.noreply.github.com>
2023-02-24 10:12:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ setup(
     url='https://github.com/pelucid/{{project_name}}',
     install_requires=requirements,
     setup_requires=['pytest-runner'],
-    tests_require=['pytest~=6.2.1', 'pytest-cov', 'tomli==1.2.2', 'coverage==6.2'],  # Coverage pinned due to AXE-2056
+    tests_require=['pytest~=6.2.1', 'pytest-cov',],
 
     entry_points={
         'console_scripts': [
             ('deploy_client_facing_info='
              'gi_feature_definitions.console_scripts.deploy_client_facing_info:main'),
-        ]
+        ] #To be modified 
     },
     classifiers= ['Programming Language :: Python :: 3.9']
 )
